bc2cb9a40cb164c74e54ac564cd1c5b5aad0df0f
Dylan Montgomery <mail@citizensinspace.com>
2015-05-07 20:38:11 -0400

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Serpscan::Dashboard::Engine.routes.draw do
-  ##mount Serpscan::Dashboard::Engine => "/serpscan"
-  #namespace :serpscan do
-    get '/' => 'websites#index', as: :websites
-    get '/:id' => 'websites#show', as: :website
-    post '/:id/keyword' => 'keywords#create'
-    get '/keywords/delete' => 'keywords#delete'
-  #end
+  get '/' => 'websites#index', as: :websites
+  get '/:id' => 'websites#show', as: :website
+  post '/:id/keyword' => 'keywords#create'
+  get '/keywords/delete' => 'keywords#delete'
 end
